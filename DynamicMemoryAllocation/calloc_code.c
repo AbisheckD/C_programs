fad57fd2b3a24677392e97edb26dfa1e0b7f2430
Abisheck D <132872487+AbisheckD@users.noreply.github.com>
2025-07-10 07:21:49 +0530

    Create calloc_code.c

---
+++ DynamicMemoryAllocation/calloc_code.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    /* creating values for 5 interger values */
+    int *ptr = (int*)calloc(5,sizeof(int));
+    
+    /* condition to check memory allocation */
+    if(ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+    }
+    else
+    {
+        printf("Memory allocation successful\n");
+    }
+    
+    /* assign values to ptr */
+    for(int i=0; i<5; i++)
+    {
+        ptr[i]=i+1;
+    }
+    
+    /* print values of ptr */
+    for(int i=0; i<5; i++)
+    {
+        printf("%d ",ptr[i]);
+    }
+    return 0;
+}
