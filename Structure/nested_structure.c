10f59bb4c7ef79740a2985694b77182dd1e75296
Abisheck D <132872487+AbisheckD@users.noreply.github.com>
2025-07-30 07:19:13 +0530

    Create nested_structure.c

---
+++ Structure/nested_structure.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+struct st1{
+    int x;
+    struct st2{
+        int y;
+    }x2;
+}x1 = {1,(2)};
+
+int main()
+{
+    printf("Size of x1 = %d\n",sizeof(x1));
+    printf("value of x = %d\n",x1.x);
+    printf("addrress of x1 = %d\n",&x1);  /* Both address of x1 and x1.x are same */
+    printf("address of x = %d\n",&x1.x);  
+    printf("Size of x2 = %d\n",sizeof(x1.x2));
+    printf("value of y = %d\n",x1.x2.y);
+    printf("address of x2 = %d\n",&x1.x2);  /* Both address of x1.x2 and x1.x2.y are same */
+    printf("address of y = %d\n",&x1.x2.y);
+    return 0;
+}
+
+/* OUTPUT */
+/*
+Size of x1 = 8
+value of x = 1
+addrress of x1 = 4210712
+address of x = 4210712
+Size of x2 = 4
+value of y = 2
+address of x2 = 4210716
+address of y = 4210716
+*/
