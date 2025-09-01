dd813853de0d0c11e4a3fea5d68706c0996f3cad
Abisheck D <132872487+AbisheckD@users.noreply.github.com>
2025-09-01 14:13:15 +0530

    Create Printing_Tokens_15.c

---
+++ Hankerrank_questions/Medium_Array_and_String/Printing_Tokens_15.c
@@ -0,0 +1,36 @@
+/* Problem number 15 in Hackerrank named "Printing Tokens" in Medium under Array and Strings */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    int i;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    //Write your logic to print the tokens of the sentence here.
+    
+    for(i = 0; s[i]; i++)
+    {
+        printf("%c",s[i]);
+        if(s[i] == ' ')
+        {
+            printf("\n");
+        }
+    }
+    return 0;
+}
+
+/* OUTPUT
+STDIN INPUT
+This is C
+
+EXPECTED OUTPUT
+This 
+is 
+C 
+*/
